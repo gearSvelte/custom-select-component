dea51d9e5d2d5c3dda6a4e95b84fc974691e4d7c
Valeriy Semenenko <gearmobile@gmail.com>
2025-05-07 20:18:24 +0300

    init project

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Select from './lib/Select.svelte';
+  import type { TOption } from './types/option.interface';
+
+  const options: TOption[] = [
+    '',
+    'red',
+    { label: 'green' },
+    { label: 'blue', value: 'b' },
+  ];
+  let selected: string | null = null;
+  const handleSelect = (event: CustomEvent) => (selected = event.detail);
+</script>
+
+<main>
+  <h1>Custom Select Element</h1>
+
+  <Select {options} on:select={handleSelect} />
+  {#if selected !== null}
+    <div class="selected">
+      You selected: <span class="selected-value">{selected}</span>
+    </div>
+  {/if}
+</main>
+
+<style lang="scss">
+  .selected {
+    margin-top: 16px;
+  }
+  .selected-value {
+    font-weight: 700;
+  }
+</style>
---
+++ src/lib/Select.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import type { TOption } from '../types/option.interface';
+  const dispatch = createEventDispatcher();
+  export let options;
+
+  const getLabel = (option: TOption) =>
+    typeof option === 'object' ? option.label : option;
+
+  const getValue = (option: TOption) =>
+    typeof option === 'object' ? option.value || option.label : option;
+</script>
+
+<select
+  on:change={(event) =>
+    dispatch('select', (event.target as HTMLSelectElement).value)}
+>
+  {#each options as option}
+    <option value={getValue(option)}>{getLabel(option)}</option>
+  {/each}
+</select>
+
+<style lang="scss"></style>
